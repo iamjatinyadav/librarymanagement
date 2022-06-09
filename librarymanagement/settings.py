cf64171815501322d05ea7985a17954df70a626d
JATIN YADAV <jatinyadav0000@gmail.com>
2022-06-09 13:06:12 +0530

    try to fatch data from databases

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lxa+7!_1$#mkrid6w@!rfz5dt@*pd=!&*f9eg6!nbj^a+i^!xi'
-
+SECRET_KEY ="django-insecure-lxa+7!_1$#mkrid6w@!rfz5dt@*pd=!&*f9eg6!nbj^nbj^a+i^!xi"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'main.apps.MainConfig',
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
+from certifi import contents
 from django.shortcuts import render, HttpResponse
-
+from main.models import Student
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/home.html')
+    allstudent = Student.objects.all()
+    context = {'allstudent':allstudent}
+    return render(request, 'main/home.html', context)
