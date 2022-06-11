847162167baa033ab665937efa76863744caffa4
JATIN YADAV <jatinyadav0000@gmail.com>
2022-06-11 18:31:26 +0530

    store seat no in a list than store a values in 1,70 in a list and get a list for seat no allocation

--- main/views.py
+++ main/views.py
@@ -4,9 +4,24 @@ from main.models import Student
 
 # Create your views here.
 def home(request):
+    list=[]
+    list2=[]
+    for i in range(1,71):
+        list.append(i)
+    print(list)
     allstudent = Student.objects.all()
-    seatno = Student.objects.raw('SELECT seatno FROM main_Student')
-    context = {'allstudent':allstudent,}
+    s=Student.objects.all().values_list('seatno')
+    for i in s:
+        list2.append(int(i[0]))
+    print(list2)    
+    for y in list2:
+        for x in list:
+            if y==x:
+                list.remove(y)
+                break
+    print(list)
+            
+    context = {'allstudent':allstudent,'i':i}
     return render(request, 'main/home.html', context)
 
     
