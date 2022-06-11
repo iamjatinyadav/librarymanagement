44e52b0cea376d094a9145a04e700c66f41b1a7c
JATIN YADAV <jatinyadav0000@gmail.com>
2022-06-11 20:57:20 +0530

    for me a small challangeing part is done where i check data in database and show remaining seats

--- main/views.py
+++ main/views.py
@@ -8,20 +8,17 @@ def home(request):
     list2=[]
     for i in range(1,71):
         list.append(i)
-    print(list)
     allstudent = Student.objects.all()
     s=Student.objects.all().values_list('seatno')
     for i in s:
-        list2.append(int(i[0]))
-    print(list2)    
+        list2.append(int(i[0]))    
     for y in list2:
         for x in list:
             if y==x:
                 list.remove(y)
                 break
-    print(list)
             
-    context = {'allstudent':allstudent,'i':i}
+    context = {'allstudent':allstudent,'seatavailable':list}
     return render(request, 'main/home.html', context)
 
     
